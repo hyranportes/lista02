3bad6fabbdd724b92bb04f8783bedb4d5fe78470
Hyran <hyran.1301706@fapi-pinhais.edu.br>
2018-03-23 21:59:29 -0300

    Pneus estão sendo fabricados Getters/Setters criados

--- src/main/java/fapi/lpoo/lista02/pneu/FabricaPneu.java
+++ src/main/java/fapi/lpoo/lista02/pneu/FabricaPneu.java
@@ -1,4 +1,48 @@
 package fapi.lpoo.lista02.pneu;
 
 public class FabricaPneu {
+
+    private String nome;
+    private int quantidadeFuncionarios;
+    private String cnpj;
+
+    public Pneu fabricarPneu() {
+        return new Pneu();
+    }
+
+    public Pneu fabricarPneu(int largura) {
+        return new Pneu(largura);
+    }
+
+    public Pneu fabricarPneu(int largura, String material) {
+        return new Pneu(largura, material);
+    }
+
+    public Pneu fabricarPneu(int largura, String material, int durabilidade) {
+        return new Pneu(largura, material, durabilidade);
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
--- src/main/java/fapi/lpoo/lista02/pneu/Pneu.java
+++ src/main/java/fapi/lpoo/lista02/pneu/Pneu.java
@@ -1,4 +1,43 @@
 package fapi.lpoo.lista02.pneu;
 
 public class Pneu {
+
+    private int largura = 30;
+    private String material = "Borracha";
+    private int durabilidade = 15000;
+
+    public Pneu() {
+
+    }
+
+    public Pneu(int largura) {
+        this.largura = largura;
+    }
+
+    public Pneu(int largura, String material) {
+        this.largura = largura;
+        this.material = material;
+    }
+
+    public Pneu(int largura, String material, int durabilidade) {
+        this.largura = largura;
+        this.material = material;
+        this.durabilidade = durabilidade;
+    }
+
+    public int getLargura() {
+        return largura;
+    }
+
+    public void setLargura(int largura) {
+        this.largura = largura;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
