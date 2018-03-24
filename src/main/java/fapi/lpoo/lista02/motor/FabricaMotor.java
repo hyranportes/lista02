1b8ea5a9c35570159807d4532b9fe091ea17245c
Hyran <hyran.1301706@fapi-pinhais.edu.br>
2018-03-23 22:22:34 -0300

    Motores estão sendo fabricados Getters/Setters criados

--- src/main/java/fapi/lpoo/lista02/motor/FabricaMotor.java
+++ src/main/java/fapi/lpoo/lista02/motor/FabricaMotor.java
@@ -1,4 +1,36 @@
 package fapi.lpoo.lista02.motor;
 
 public class FabricaMotor {
+
+    private String nome;
+    private int quantidadeFuncionarios;
+    private String cnpj;
+
+    public fabricarMotor(float potencia, int peso, String material){
+        return new Motor (potencia, peso, material);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
 }
--- src/main/java/fapi/lpoo/lista02/motor/Motor.java
+++ src/main/java/fapi/lpoo/lista02/motor/Motor.java
@@ -1,4 +1,15 @@
 package fapi.lpoo.lista02.motor;
 
 public class Motor {
+
+    private double potencia = 2.0;
+    private int peso = 300;
+    private String material = "titanio";
+
+
+    public Motor(double potencia, int peso, String material) {
+        this.potencia = potencia;
+        this.peso = peso;
+        this.material = material;
+    }
 }
