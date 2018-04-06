900812c70a6d452aa996a0daaa5a29f60b467073
Hyran <hyran.1301706@fapi-pinhais.edu.br>
2018-04-05 21:10:35 -0300

    Inicio do carro 3 Getters/Setters criados

--- src/main/java/fapi/lpoo/lista02/carro/Carro.java
+++ src/main/java/fapi/lpoo/lista02/carro/Carro.java
@@ -1,6 +1,9 @@
 package fapi.lpoo.lista02.carro;
 
+import fapi.lpoo.lista02.banco.Banco;
 import fapi.lpoo.lista02.motor.Motor;
+import fapi.lpoo.lista02.opcionais.Bluetooth;
+import fapi.lpoo.lista02.opcionais.TetoSolar;
 import fapi.lpoo.lista02.pneu.Pneu;
 import fapi.lpoo.lista02.porta.Porta;
 import fapi.lpoo.lista02.roda.Roda;
@@ -13,12 +16,42 @@ public class Carro {
     private Porta porta;
     private Roda roda;
     private Tanque tanque;
+    private Banco banco[] = new Banco[3];
+    private TetoSolar tetoSolar;
+    private Bluetooth bluetooth;
+    private Pneu step;
 
-    public Carro(Motor motor, Pneu[] pneu, Porta porta, Roda roda, Tanque tanque) {
+    public Carro(Motor motor, Pneu[] pneu, Porta porta, Roda roda, Tanque tanque, Banco[] banco) {
         this.motor = motor;
         this.pneu = pneu;
         this.porta = porta;
         this.roda = roda;
         this.tanque = tanque;
+        this.banco = banco;
     }
+
+    public TetoSolar getTetoSolar() {
+        return tetoSolar;
+    }
+
+    public void setTetoSolar(TetoSolar tetoSolar) {
+        this.tetoSolar = tetoSolar;
+    }
+
+    public Bluetooth getBluetooth() {
+        return bluetooth;
+    }
+
+    public void setBluetooth(Bluetooth bluetooth) {
+        this.bluetooth = bluetooth;
+    }
+
+    public Pneu getStep() {
+        return step;
+    }
+
+    public void setStep(Pneu step) {
+        this.step = step;
+    }
+
 }
--- src/main/java/fapi/lpoo/lista02/carro/FabricaCarro.java
+++ src/main/java/fapi/lpoo/lista02/carro/FabricaCarro.java
@@ -4,20 +4,31 @@ import fapi.lpoo.lista02.motor.FabricaMotor;
 import fapi.lpoo.lista02.motor.Motor;
 import fapi.lpoo.lista02.pneu.FabricaPneu;
 import fapi.lpoo.lista02.pneu.Pneu;
+import fapi.lpoo.lista02.porta.FabricaPorta;
+import fapi.lpoo.lista02.porta.Porta;
+import fapi.lpoo.lista02.tanque.Tanque;
 
 public class FabricaCarro {
+
     public Carro fabricarCarro(){
         FabricaMotor fabricaMotor = new FabricaMotor();
         Motor motor = fabricaMotor.fabricarMotor(100,100,"Ferro","1");
 
         FabricaPneu fabricaPneu = new FabricaPneu();
-        Pneu pneu[] = new pneu[4];
+        Pneu pneu[] = new Pneu[4];
 
         pneu[0] = fabricaPneu.fabricarPneu();
         pneu[1] = fabricaPneu.fabricarPneu();
         pneu[2] = fabricaPneu.fabricarPneu();
         pneu[3] = fabricaPneu.fabricarPneu();
 
-        return new Carro(motor,pneu,porta,roda,tanque);
+        FabricaPorta fabricaPorta = new FabricaPorta();
+        Porta porta = new FabricaPorta();
+
+        FabricaTanque fabricaTanque = new FabricaTanque();
+        Tanque tanque = fabricaTanque
+
+        Carro carro =  new Carro(motor,pneu,porta,roda,tanque, banco);
+
     }
 }
